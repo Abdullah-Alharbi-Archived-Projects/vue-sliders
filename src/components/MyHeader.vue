f3b81a02eb032aebdc3fc8fca434b5090ea7ecd2
W2AlharbiMe <w2.alharbi.me@gmail.com>
2019-09-17 10:38:44 +0300

    my-header component

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h1>Welcome to our site</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "my-header"
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+div {
+  height: 100%;
+}
+</style>
